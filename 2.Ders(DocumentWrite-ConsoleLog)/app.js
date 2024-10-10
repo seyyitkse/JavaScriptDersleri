dbcffafc8ed7c9b0f67f320efc72d284613e7be8
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-10-10 21:27:32 +0300

    Add initial files for lesson-2-3

---
+++ 2.Ders(DocumentWrite-ConsoleLog)/app.js
@@ -0,0 +1,21 @@
+document.write("Merhaba Ahmet Seyyit, Bugün nasilsin?")
+
+console.log("Hello World");
+
+alert("Merhaba")
+
+
+// window.localStorage
+
+// localStorage
+
+// document.createElement
+
+
+//burası yorum alanı
+
+
+/*
+Merhaba burası da yorum alanı
+ */
+
